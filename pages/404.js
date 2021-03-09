0077160963a9c35110fc0e382a0746217d8d4158
Victoria <victoria.ancalaf@gmail.com>
2021-03-08 23:13:13 -0300

    Nextjs pages: 404, about, home & components

---
+++ pages/404.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
+
+const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 5000);
+  }, []);
+
+  return (
+    <div className="not-found">
+      <h1>Oooops...</h1>
+      <h2>That page cannot be found.</h2>
+      <Image src="/run.png" alt="Go back to the Homepage" width={240} height={160} />
+      <p>
+        Go back to the <Link href="/">Homepage</Link>
+      </p>
+    </div>
+  );
+};
+
+export default NotFound;
---
+++ pages/lemon/index.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Lemon.module.css";
+
+const Lemon = () => {
+  return (
+    <div>
+      <h1>All Lemons</h1>
+    </div>
+  );
+};
+
+export default Lemon;
