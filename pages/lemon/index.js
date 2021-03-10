cb4f1e8615a3c2c50e08f72964c50036c9ac6506
Victoria <victoria.ancalaf@gmail.com>
2021-03-09 22:22:13 -0300

    fetching data and creating dynamic routes

--- pages/lemon/index.js
+++ pages/lemon/index.js
@@ -1,9 +1,26 @@
-import styles from "../styles/Lemon.module.css";
+import Link from 'next/link';
+import styles from "../../styles/Lemon.module.css";
 
-const Lemon = () => {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { lemons: data },
+  };
+};
+
+const Lemon = ({ lemons }) => {
   return (
     <div>
       <h1>All Lemons</h1>
+      {lemons.map((lemon) => (
+        <Link href={'/lemon/' + lemon.id} key={lemon.id}>
+          <a className={styles.single}>
+            <h3>{lemon.name}</h3>
+          </a>
+        </Link>
+      ))}
     </div>
   );
 };
